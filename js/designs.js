1507a12e3bd311d8915af27c7f5fd93d8d36cd74
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-05-21 18:01:34 -0300

    Select color from colorPicker.
    Select color on the initialization of page and get the color when user
    change it using a listener.

--- js/designs.js
+++ js/designs.js
@@ -2,6 +2,13 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+let color = $('#colorPicker').val();
+console.log(color);
+
+$('#colorPicker').on('change', function(){
+  color = $(this).val();
+  console.log(color);
+});
 
 function makeGrid() {
 
