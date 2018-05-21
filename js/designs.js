55eeafac6a55bfe9ec0d85394ffdf5b06a663677
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-05-21 19:19:15 -0300

    Adding color to td on click.
    Adds color to the clicked td using the color of the colorPicker, if the
    user clicks on a td with the same color the td is clean (goes to white).

--- js/designs.js
+++ js/designs.js
@@ -2,7 +2,7 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-let color = $('#colorPicker').val();
+let color = $('#colorPicker').val(), colorTd;
 console.log(color);
 
 $('#colorPicker').on('change', function(){
@@ -14,6 +14,7 @@ let sizeH = $('#inputHeight').val();
 let sizeW = $('#inputWeight').val();
 console.log(sizeH);
 console.log(sizeW);
+let html = [];
 makeGrid();
 
 $('#sizePicker').on('submit', function(evt){
@@ -24,6 +25,20 @@ $('#sizePicker').on('submit', function(evt){
   console.log(sizeW);
   makeGrid();
 });
+
+
+$('#pixelCanvas').on('click', 'td', function(){
+  
+  if(rgb2hex($(this).css('background-color')) == color){
+    //clean td
+    $(this).css('background-color', '#ffffff');
+  }
+  else {
+    //paint td
+    $(this).css('background-color', color);
+  }
+
+})
 
 // I believe this makeGrid() function is more intuitive, but is very slow
 // because of the constant change in the DOM
@@ -42,7 +57,6 @@ $('#sizePicker').on('submit', function(evt){
 //   console.log(performance.now()-before);
 // }
 
-let html = [];
 function makeGrid() {
   //let before = performance.now();
   $('#pixelCanvas').children().remove();
@@ -52,7 +66,7 @@ function makeGrid() {
     html.push('<tr>');
 
     for(let j = 0; j < sizeW; j++)
-      html.push('<td></td>');
+    html.push('<td></td>');
 
     html.push('</tr>');
   }
@@ -60,4 +74,17 @@ function makeGrid() {
   $("#pixelCanvas").append( html.join('') );
 
   //console.log(performance.now()-before);
+}
+
+
+// * @license WTFPL 2.0 license. (http://wtfjs.com/license)
+// * @author Fedot Praslov
+// * based on comments in StackOverflow question
+// * @link http://stackoverflow.com/questions/638948/background-color-hex-to-javascript-variable-jquery
+function hex(x) {
+  return ("0" + parseInt(x).toString(16)).slice(-2);
+}
+function rgb2hex(rgb) {
+  rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(,\s*\d+\.*\d+)?\)$/);
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
