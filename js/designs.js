befb27f42a8d76a8399e70558a37501d8cdcddb4
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-05-21 19:22:36 -0300

    Cleaning logs and ajusting position of variables, listeners and functions.

--- js/designs.js
+++ js/designs.js
@@ -1,34 +1,30 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
 let color = $('#colorPicker').val(), colorTd;
-console.log(color);
-
-$('#colorPicker').on('change', function(){
-  color = $(this).val();
-  console.log(color);
-});
 
 let sizeH = $('#inputHeight').val();
 let sizeW = $('#inputWeight').val();
-console.log(sizeH);
-console.log(sizeW);
+
 let html = [];
+
 makeGrid();
+
+
+$('#colorPicker').on('change', function(){
+  color = $(this).val();
+
+});
 
 $('#sizePicker').on('submit', function(evt){
   evt.preventDefault();
+
   sizeH = $('#inputHeight').val();
   sizeW = $('#inputWeight').val();
-  console.log(sizeH);
-  console.log(sizeW);
+
   makeGrid();
 });
 
-
 $('#pixelCanvas').on('click', 'td', function(){
-  
+
   if(rgb2hex($(this).css('background-color')) == color){
     //clean td
     $(this).css('background-color', '#ffffff');
@@ -39,6 +35,37 @@ $('#pixelCanvas').on('click', 'td', function(){
   }
 
 })
+
+function makeGrid() {
+
+  $('#pixelCanvas').children().remove();
+  html = [];
+
+  for (i = 0; i < sizeH; i++) {
+    html.push('<tr>');
+
+    for(let j = 0; j < sizeW; j++)
+    html.push('<td></td>');
+
+    html.push('</tr>');
+  }
+
+  $("#pixelCanvas").append( html.join('') );
+
+}
+
+
+// * @license WTFPL 2.0 license. (http://wtfjs.com/license)
+// * @author Fedot Praslov
+// * based on comments in StackOverflow question
+// * @link http://stackoverflow.com/questions/638948/background-color-hex-to-javascript-variable-jquery
+function hex(x) {
+  return ("0" + parseInt(x).toString(16)).slice(-2);
+}
+function rgb2hex(rgb) {
+  rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(,\s*\d+\.*\d+)?\)$/);
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
 
 // I believe this makeGrid() function is more intuitive, but is very slow
 // because of the constant change in the DOM
@@ -56,35 +83,3 @@ $('#pixelCanvas').on('click', 'td', function(){
 //   });
 //   console.log(performance.now()-before);
 // }
-
-function makeGrid() {
-  //let before = performance.now();
-  $('#pixelCanvas').children().remove();
-  html = [];
-
-  for (i = 0; i < sizeH; i++) {
-    html.push('<tr>');
-
-    for(let j = 0; j < sizeW; j++)
-    html.push('<td></td>');
-
-    html.push('</tr>');
-  }
-
-  $("#pixelCanvas").append( html.join('') );
-
-  //console.log(performance.now()-before);
-}
-
-
-// * @license WTFPL 2.0 license. (http://wtfjs.com/license)
-// * @author Fedot Praslov
-// * based on comments in StackOverflow question
-// * @link http://stackoverflow.com/questions/638948/background-color-hex-to-javascript-variable-jquery
-function hex(x) {
-  return ("0" + parseInt(x).toString(16)).slice(-2);
-}
-function rgb2hex(rgb) {
-  rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(,\s*\d+\.*\d+)?\)$/);
-  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-}
